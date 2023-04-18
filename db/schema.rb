c4b9c299d0159176d53f50141159ea83e2411ef0
Andy Pierce <spudos16@gmail.com>
2023-04-18 17:22:48 +0100

    add users and model table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_155829) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_160906) do
   create_table "setups", force: :cascade do |t|
     t.string "car"
     t.text "driver"
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_155829) do
     t.integer "frdiff"
     t.integer "ctrdiff"
     t.integer "rediff"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
