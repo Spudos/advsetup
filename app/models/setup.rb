08f1efee762a3a67b077f4116f3d2e3a06926d4f
Andy Pierce <spudos16@gmail.com>
2023-04-19 13:18:18 +0100

    saves

--- app/models/setup.rb
+++ app/models/setup.rb
@@ -2,5 +2,4 @@ class Setup < ApplicationRecord
    validates :car, presence: true, length: { minimum: 4, Maximum: 20}
    validates :driver, presence: true, length: { minimum: 4, Maximum: 25}
    validates :track, presence: true, length: { minimum: 3, Maximum: 25}
-   validates :frshk, presence: true
 end
