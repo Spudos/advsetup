f87fe180cb7fc8ac46b63eb1515ad7f9f0019732
Andy Pierce <spudos16@gmail.com>
2023-04-19 17:18:00 +0100

    user form sign up added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to RC Setup #{@user.username}, you have sucuessfully signed up"
+            redirect_to setups_path    
+        else
+            render 'new'
+        end
+    end
+private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
