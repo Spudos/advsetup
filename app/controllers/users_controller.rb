73d727cf919c4eb826f3b1f369d7f43a06aabe1e
Andy Pierce <spudos16@gmail.com>
2023-04-20 17:02:27 +0100

    added user listing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @setups = @user.setups
+    end
+    
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your profile was updated"
+            redirect_to setups_path
+        else
+            render 'edit'
+        end
     end
 
     def create
--- app/models/setup.rb
+++ app/models/setup.rb
@@ -1,4 +1,6 @@
 class Setup < ApplicationRecord
+belongs_to :user
+
    validates :car, presence: true, length: { minimum: 4, Maximum: 20}
    validates :driver, presence: true, length: { minimum: 4, Maximum: 25}
    validates :track, presence: true, length: { minimum: 3, Maximum: 25}
